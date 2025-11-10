bcf93d2a7c3feece42ac98db6b540f6494295dda
Lance Jian <lancejian@fortinet.com>
2025-11-10 14:44:58 -0800

    change python back to pyright

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,8 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "ts_ls", "basedpyright", "eslint", "gopls" }
+local servers = { "html", "cssls", "ts_ls", "eslint", "gopls", "pyright"}
+
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
