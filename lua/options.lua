a0a1080fab5147c5666234c4e42afdaebd9213fe
Lance Jian <lancejian@fortinet.com>
2026-03-26 14:52:03 -0700

    update config

--- lua/options.lua
+++ lua/options.lua
@@ -7,8 +7,6 @@ require "nvchad.options"
 
 vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
 vim.opt.grepformat = "%f:%l:%c:%m,%f"
--- vim.opt.relativenumber = true
---
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldopen = "all"
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,44 @@
-require("telescope").setup {
-
-  defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
-    mappings = {
-      n = {
-        ["<c-d>"] = require("telescope.actions").delete_buffer,
-      }, -- n
-      i = {
-        ["<C-h>"] = "which_key",
-        ["<c-d>"] = require("telescope.actions").delete_buffer,
-      }, -- i
-    }, -- mappings
-  }, -- defaults
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      version = "^1.0.0",
+    },
+  },
+  keys = {
+    -- Fixed: Wrapped in a sub-table and updated to use the extension
+    {
+      "<leader>fg",
+      function()
+        require("telescope").extensions.live_grep_args.live_grep_args()
+      end,
+      desc = "Live Grep with Args",
+    },
+  },
+  opts = function(_, opts)
+    local actions = require "telescope.actions"
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+      preview = {
+        treesitter = false,
+      },
+      mappings = {
+        n = { ["<c-d>"] = actions.delete_buffer },
+        i = {
+          ["<C-h>"] = "which_key",
+          ["<c-d>"] = actions.delete_buffer,
+        },
+      },
+    })
+  end,
+  config = function(_, opts)
+    local telescope = require "telescope"
+    telescope.setup(opts)
+    telescope.load_extension "live_grep_args"
+    -- Note: LazyVim usually handles fzf-native loading automatically,
+    -- but you can add telescope.load_extension("fzf") here if needed.
+  end,
 }
-
-return {}
